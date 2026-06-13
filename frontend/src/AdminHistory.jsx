4f4e38c535c68aaf17b159a8677c4feea24e1236
Ibrahimkhan1034 <ibrahim904072@gmail.com>
2026-06-13 16:32:24 +0500

    change the link from local to vercel

--- frontend/src/AdminHistory.jsx
+++ frontend/src/AdminHistory.jsx
@@ -8,7 +8,9 @@ const AdminHistory = () => {
   useEffect(() => {
     const fetchRecords = async () => {
       try {
-        const response = await axios.get("http://localhost:4000/api/records");
+        const response = await axios.get(
+          "https://student-early-dropout-prediction.vercel.app/api/records",
+        );
         setRecords(response.data);
       } catch (error) {
         console.error("Error fetching records:", error);
